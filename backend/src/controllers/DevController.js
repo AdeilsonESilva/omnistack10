c2cba67cb8f51d28fa66837b2a4e38698ed1e146
AdeilsonESilva <adeilson@gmail.com>
2020-01-14 22:31:48 -0300

    - Atualizar um dev - Excluir um dev

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -39,5 +39,59 @@ module.exports = {
     }
 
     return res.json(dev);
-  }
+  },
+
+  async update(req, res) {
+    const { github_username, techs, latitude, longitude } = req.body;
+
+    let dev = await Dev.findOne({ github_username });
+
+    if (!dev) {
+      res.status(404).json({message: 'Developer not found!'});
+    }
+
+    const apiResp = await axios.get(
+      `https://api.github.com/users/${github_username}`
+    );
+
+    const { name = login, avatar_url, bio } = apiResp.data;
+
+    const techsArray = parseStringAsArray(techs);
+
+    const location = {
+      type: "Point",
+      coordinates: [longitude, latitude]
+    };
+
+    await Dev.update({github_username}, {
+      name,
+      avatar_url,
+      bio,
+      techs: techsArray,
+      location
+    });
+
+    dev = await Dev.findOne({ github_username });
+  
+    return res.json(dev);
+  },
+  
+  async destroy(req, res) {
+    const { github_username } = req.body;
+
+    let dev = await Dev.findOne({ github_username });
+
+    if (!dev) {
+      res.status(404).json({message: 'Developer not found!'});
+    }
+
+    dev = await Dev.deleteOne({github_username});
+
+    if (dev) {
+      return res.json({message: 'Developer deleted success!!'});
+    }
+
+    return res.status(400).json({message: 'Error on delete developer!'});
+    
+  },
 };
