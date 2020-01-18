38b5aefefb54a9110319f04563d7ab70280c17dd
AdeilsonESilva <adeilson@gmail.com>
2020-01-17 23:22:45 -0300

    - Adicionado websocket no backend

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const Dev = require("../models/Dev");
 const parseStringAsArray = require("../utils/parseStringAsArray");
+const { findConnections, sendMessage } = require("../websocket");
 
 module.exports = {
   async index(req, res) {
@@ -36,6 +37,16 @@ module.exports = {
         techs: techsArray,
         location
       });
+
+      const sendSocketMessageTo = findConnections(
+        {
+          latitude,
+          longitude
+        },
+        techsArray
+      );
+
+      sendMessage(sendSocketMessageTo, 'new-dev', dev);
     }
 
     return res.json(dev);
@@ -47,7 +58,7 @@ module.exports = {
     let dev = await Dev.findOne({ github_username });
 
     if (!dev) {
-      res.status(404).json({message: 'Developer not found!'});
+      res.status(404).json({ message: "Developer not found!" });
     }
 
     const apiResp = await axios.get(
@@ -63,35 +74,37 @@ module.exports = {
       coordinates: [longitude, latitude]
     };
 
-    await Dev.update({github_username}, {
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray,
-      location
-    });
+    await Dev.update(
+      { github_username },
+      {
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      }
+    );
 
     dev = await Dev.findOne({ github_username });
-  
+
     return res.json(dev);
   },
-  
+
   async destroy(req, res) {
     const { github_username } = req.body;
 
     let dev = await Dev.findOne({ github_username });
 
     if (!dev) {
-      res.status(404).json({message: 'Developer not found!'});
+      res.status(404).json({ message: "Developer not found!" });
     }
 
-    dev = await Dev.deleteOne({github_username});
+    dev = await Dev.deleteOne({ github_username });
 
     if (dev) {
-      return res.json({message: 'Developer deleted success!!'});
+      return res.json({ message: "Developer deleted success!!" });
     }
 
-    return res.status(400).json({message: 'Error on delete developer!'});
-    
-  },
+    return res.status(400).json({ message: "Error on delete developer!" });
+  }
 };
